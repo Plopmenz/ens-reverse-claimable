5994800746833a4b846f0dde5ee5c1f3d8976115
Plopmenz <plopmenz@gmail.com>
2024-02-01 00:46:45 +0100

    make contract abstract to be clear its not meant to be deployed as is

--- src/ClaimReverseENS.sol
+++ src/ClaimReverseENS.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import {IReverseRegistrar} from "../lib/ens-contracts/contracts/reverseRegistrar/IReverseRegistrar.sol";
 
-contract ClaimReverseENS {
+abstract contract ClaimReverseENS {
     /// @dev Set reverseRegistrar to zero address for no claim (for example on unsupported networks).
     constructor(address reverseRegistrar, address claimTo) {
         if (reverseRegistrar != address(0)) {
